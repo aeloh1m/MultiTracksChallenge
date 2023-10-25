4741064f0cf96e5000475d8efacba3882735242e
aeloh1m <melowgz@gmail.com>
2023-10-25 00:53:06 -0300

    Saving implementation in another branch

--- DB/Stored Procedures/GetArtistDetails.sql
+++ DB/Stored Procedures/GetArtistDetails.sql
@@ -2,8 +2,9 @@ CREATE PROCEDURE GetArtistDetails
 AS
 BEGIN 
 
-  SELECT DISTINCT AR.artistID, AR.title as artistName, AL.title as albumName, SO.title as songName, AR.imageURL as artistIMG, AL.imageURL as albumIMG, SO.bpm FROM Artist AR 
+  SELECT DISTINCT AR.artistID, AR.title as artistName, AL.title as albumName, SO.title as songName, AR.imageURL as artistIMG, AR.heroURL as artistBanner, AL.imageURL as albumIMG, SO.bpm, AR.biography FROM Artist AR 
   LEFT JOIN Album AL ON AR.artistID = AL.artistID
   LEFT JOIN Song SO ON AL.artistID=SO.artistID
+
 
 END ;
--- Web/multitracks.com/multitracks.com/PageToSync/artistDetails.aspx.cs
+++ Web/multitracks.com/multitracks.com/PageToSync/artistDetails.aspx.cs
@@ -1,5 +1,7 @@
 using DataAccess;
 using System;
+using System.Data;
+using System.Linq;
 using System.Web.UI.WebControls;
 
 public partial class Default : MultitracksPage
@@ -9,17 +11,87 @@ public partial class Default : MultitracksPage
     {
         var sql = new SQL();
 
-        try
+        if (!IsPostBack)
         {
-            //sql.Parameters.Add("@stepID", 1);
-            var data = sql.ExecuteStoredProcedureDT("GetArtistDetails");
+            string artistID = Request.QueryString["artistID"];
 
-            GetArtistDetails.DataSource = data;
-            GetArtistDetails.DataBind();
+            if (!string.IsNullOrEmpty(artistID))
+            {
+                // Convert artistID to the desired data type (e.g., int)
+                int targetArtistID;
+                if (int.TryParse(artistID, out targetArtistID))
+                {
+                    var data = sql.ExecuteStoredProcedureDT("GetArtistDetails");
+                    // Use artistIDValue to retrieve and display artist details
+                    // Perform the database query using artistIDValue
+                    // Display the artist details on the page
 
+                    //========= Artist and Song field  =========//
+                    var artistAndSongData = data.AsEnumerable()
+                        .Where(row => row.Field<int>("artistID") == targetArtistID)
+                        .Select(row => new
+                        {
+                            AlbumIMG = row.Field<string>("albumIMG"),
+                            AlbumName = row.Field<string>("albumName"),
+                            ArtistName = row.Field<string>("artistName"),
+                            ArtistBanner = row.Field<string>("artistBanner"),
+                            SongName = row.Field<string>("songName"),
+                            Bpm = row.Field<decimal>("bpm")
+
+
+
+                        })
+                        .ToList();
+
+                    GetArtistAndSong.DataSource = artistAndSongData;
+                    GetArtistAndSong.DataBind();
+
+                    bpmLabel.Text = artistAndSongData.BPM.ToString();
+                    albumImage.ImageUrl = artistAndSongData.AlbumIMG;
+                    artistBanner.ImageUrl = artistAndSongData.ArtistBanner;
+
+
+                    //========= Album field  =========//
+                    var albumData = data.AsEnumerable()
+                        .Where(row => row.Field<int>("artistID") == targetArtistID)
+                        .Select(row => new
+                        {
+                            AlbumIMG = row.Field<string>("albumIMG"),
+                            AlbumName = row.Field<string>("albumName"),
+                            ArtistName = row.Field<string>("artistName")
+                        })
+                        .ToList();
+
+                    // Now 'albumData' contains the desired fields
+
+                    // You can bind this data to a control or display it as needed
+                    GetAlbumDetails.DataSource = albumData;
+                    GetAlbumDetails.DataBind();
+                    //========= Biography field  =========//
+
+                    var biography = data.AsEnumerable()
+                    .Where(row => row.Field<int>("artistID") == targetArtistID)
+                    .Select(row => row.Field<string>("biography"))
+                    .FirstOrDefault(); // Assuming there is one biography per artist
+
+                    // Now 'biography' contains the desired biography
+
+                    // You can display it or use it as needed
+                    biographyLabel.Text = biography;
+
+                }
+                else
+                {
+                    // Handle invalid artistID
+                    //invalidArtist.Visible = true;
+                }
+            }
+            else
+            {
+                // Handle missing artistID
+                // error505.Visible = true;
+            }
         }
-        catch
-        {
-        }
+
     }
 }
