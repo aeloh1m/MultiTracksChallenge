7ab3fada71b358a089d450abcf94ff6626975276
aeloh1m <melowgz@gmail.com>
2023-10-25 23:07:44 -0300

    Merge squashing newImplementationIdea#1 branch

--- Web/multitracks.com/multitracks.com/PageToSync/artistDetails.aspx.cs
+++ Web/multitracks.com/multitracks.com/PageToSync/artistDetails.aspx.cs
@@ -1,5 +1,9 @@
 using DataAccess;
+using DocumentFormat.OpenXml.Drawing;
 using System;
+using System.Data;
+using System.Linq;
+using System.Reflection.Emit;
 using System.Web.UI.WebControls;
 
 public partial class Default : MultitracksPage
@@ -9,17 +13,98 @@ public partial class Default : MultitracksPage
     {
         var sql = new SQL();
 
-        try
+        if (!IsPostBack)
         {
-            //sql.Parameters.Add("@stepID", 1);
-            var data = sql.ExecuteStoredProcedureDT("GetArtistDetails");
+            string artistID = Request.QueryString["artistID"];
 
-            GetArtistDetails.DataSource = data;
-            GetArtistDetails.DataBind();
+            if (!string.IsNullOrEmpty(artistID))
+            {
+                // Catch and convert artistID to int 
+                int targetArtistID;
 
+                if (int.TryParse(artistID, out targetArtistID))
+                {
+                    /*
+                     Data gets parsed with the artistID given via URL, example:
+                    http://localhost:56916/PageToSync/artistDetails.aspx?artistID=107
+                     */
+                    var data = sql.ExecuteStoredProcedureDT("GetArtistDetails");
+
+                    //========= Artist field  =========//
+                    var getArtistDetailsQuery = (from row in data.AsEnumerable()
+                                                 where row.Field<int>("artistID") == targetArtistID
+                                                 select new
+                                                 {
+                                                     ArtistBanner = row.Field<string>("artistBanner"),
+                                                     ArtistIMG = row.Field<string>("artistIMG"),
+                                                     ArtistName = row.Field<string>("artistName")
+                                                 }).FirstOrDefault(); // Assuming there's only one result for the artist
+                    if (getArtistDetailsQuery != null)
+                    {
+                        artistBannerImage.ImageUrl = getArtistDetailsQuery.ArtistBanner.ToString();
+                        artistImage.ImageUrl = getArtistDetailsQuery.ArtistIMG.ToString();
+                        artistNameLabel.Text = getArtistDetailsQuery.ArtistName.ToString();
+                    }
+
+                    //========= Artist and Song field  =========//
+                    var getArtistAndSongDetailsQuery = from row in data.AsEnumerable()
+                                                       where row.Field<int>("artistID") == targetArtistID
+                                                       select new
+                                                       {
+                                                           AlbumIMG = row.Field<string>("albumIMG"),
+                                                           SongName = row.Field<string>("songName"),
+                                                           AlbumName = row.Field<string>("albumName"),
+                                                           BPM = row.Field<decimal>("bpm").ToString(),
+                                                           TimeSignature = row.Field<string>("timeSignature") // Include timeSignature
+                                                       };
+
+                    var artistAndSongQueryList = getArtistAndSongDetailsQuery.ToList();
+
+                    getArtistAndSongDetails.DataSource = artistAndSongQueryList;
+                    getArtistAndSongDetails.DataBind();
+
+                    //========= Album field  =========//
+                    var getAlbumDetailsQuery = (from row in data.AsEnumerable()
+                                                where row.Field<int>("artistID") == targetArtistID
+                                                select new
+                                                {
+                                                    AlbumIMG = row.Field<string>("albumIMG"),
+                                                    ArtistName = row.Field<string>("artistName"),
+                                                    AlbumName = row.Field<string>("albumName")
+                                                }).Distinct();
+
+                    var albumDetailsQueryList = getAlbumDetailsQuery.ToList();
+
+                    getAlbumDetails.DataSource = albumDetailsQueryList;
+                    getAlbumDetails.DataBind();
+
+                    //========= Biography field  =========//
+                    var biographyData = data.AsEnumerable()
+                        .Where(row => row.Field<int>("artistID") == targetArtistID)
+                        .Select(row => new
+                        {
+                            Biography = row.Field<string>("biography")
+                        })
+                        .FirstOrDefault();
+
+                    if (biographyData != null)
+                    {
+                        // Set the biography text to the Label control
+                        biographyLabel.Text = biographyData.Biography;
+                    }
+
+                    invalidArtist.Visible = false;
+                    displayData.Visible = true;
+
+                }
+                else
+                {
+                    // Handle invalid artistID
+                    invalidArtist.Visible = true;
+                    displayData.Visible = false;
+                }
+            }
         }
-        catch
-        {
-        }
+
     }
 }
